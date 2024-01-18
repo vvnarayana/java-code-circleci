7b8d17e404c6b4f7fb462ae67577e036576978cb
Narayana <veera.v@trundl.com>
2024-01-18 18:37:57 +0530

    Dynamic Build Artifact

--- update_jira.sh
+++ update_jira.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 JIRA_URL="https://veerav.atlassian.net/rest/api/2/issue/CI-4"
-ARTIFACT_LINK=$(grep ARTIFACT_LINK $BASH_ENV | cut -d'=' -f2)
+ARTIFACT_LINK=$(grep ARTIFACT_LINK /tmp/artifacts/$BASH_ENV_FILENAME | cut -d'=' -f2)
 
 curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X PUT -H "Content-Type: application/json" \
      -d "{\"update\": {\"comment\": [{\"add\": {\"body\": \"Latest Artifact link: $ARTIFACT_LINK\"}}]}}" \
