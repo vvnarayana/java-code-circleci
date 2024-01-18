5b454e01d5b229bf61a04181e6eefad0bff11b0a
Narayana <veera.v@trundl.com>
2024-01-18 17:22:19 +0530

    Aritifact Upload

--- update_jira.sh
+++ update_jira.sh
@@ -3,10 +3,7 @@
 JIRA_URL="https://veerav.atlassian.net/rest/api/2/issue/CI-4"
 ARTIFACT_LINK="https://output.circle-artifacts.com/output/job/d7e80c45-3011-47a7-a2b0-535aba1968bf/artifacts/0/tmp/artifacts/jiracon22-1.0.0.war"
 
-# Extract the issue key from the JIRA URL
-ISSUE_KEY=$(basename $JIRA_URL)
-
-curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X POST -H "Content-Type: application/json" \
-     -d "{\"type\":{\"name\":\"blocks\"},\"inwardIssue\":{\"key\":\"$ISSUE_KEY\"},\"outwardIssue\":{\"key\":\"$ISSUE_KEY\"}}" \
-     $JIRA_URL/links
+curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X PUT -H "Content-Type: application/json" \
+     -d "{\"update\": {\"comment\": [{\"add\": {\"body\": \"Artifact link: $ARTIFACT_LINK\"}}]}}" \
+     $JIRA_URL
 
