6896e726cf2efc112d0e9386b7958034686634aa
Narayana <veera.v@trundl.com>
2024-01-23 16:22:40 +0530

    gitlab

--- update_jira.sh
+++ update_jira.sh
@@ -2,17 +2,17 @@
 
 JIRA_URL="https://veerav.atlassian.net/rest/api/2/issue/CI-4"
 ARTIFACT_LOCATION="/tmp/artifacts/jiracon22-1.0.0.war"
+JIRA_API_TOKEN="YOUR_JIRA_API_TOKEN"  # Replace with your actual Jira API token
 
 # Check if the artifact file exists
 if [ -f "$ARTIFACT_LOCATION" ]; then
     # Construct the comment body
+    COMMENT_BODY="Latest Artifact link: [jiracon22-1.0.0.war](${ARTIFACT_LOCATION})"
     
     # Make the Jira API call to update the comment
     curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X PUT -H "Content-Type: application/json" \
          -d "{\"update\": {\"comment\": [{\"add\": {\"body\": \"$COMMENT_BODY\"}}]}}" \
          "$JIRA_URL"
-
-     COMMENT_BODY="Latest Artifact link: [jiracon22-1.0.0.war](${ARTIFACT_LOCATION})"
 else
     echo "Artifact file not found at: $ARTIFACT_LOCATION"
 fi
