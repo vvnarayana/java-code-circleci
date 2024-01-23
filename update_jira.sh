a5934d7a792895497ff636def56bb1339bdd013a
Narayana <veera.v@trundl.com>
2024-01-23 16:06:55 +0530

    Updated update.sh

--- update_jira.sh
+++ update_jira.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 JIRA_URL="https://veerav.atlassian.net/rest/api/2/issue/CI-4"
-ARTIFACT_LINK=$(grep ARTIFACT_LINK /tmp/artifacts/$BASH_ENV_FILENAME | cut -d'=' -f2)
+ARTIFACT_LOCATION="/tmp/artifacts/jiracon22-1.0.0.war"
 
-curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X PUT -H "Content-Type: application/json" \
-     -d "{\"update\": {\"comment\": [{\"add\": {\"body\": \"Latest Artifact link: $ARTIFACT_LINK\"}}]}}" \
-     $JIRA_URL
+# Check if the artifact file exists
+if [ -f "$ARTIFACT_LOCATION" ]; then
+    # Construct the comment body
+    COMMENT_BODY="Latest Artifact link: [jiracon22-1.0.0.war](${ARTIFACT_LOCATION})"
+
+    # Make the Jira API call to update the comment
+    curl -u "veera.v@trundl.com:$JIRA_API_TOKEN" -X PUT -H "Content-Type: application/json" \
+         -d "{\"update\": {\"comment\": [{\"add\": {\"body\": \"$COMMENT_BODY\"}}]}}" \
+         "$JIRA_URL"
+else
+    echo "Artifact file not found at: $ARTIFACT_LOCATION"
+fi
